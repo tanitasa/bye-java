11e722f50a18d93a0914b84626463cd2b98804f9
Manuel Resinas <resinas@us.es>
2018-01-15 08:55:03 +0100

    Changes

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -6,12 +6,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
